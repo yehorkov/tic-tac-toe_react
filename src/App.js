f9dfdbbb7f5976a4863b5bc255324a6c1c462d80
Yehor <yehor1koval@gmail.com>
2023-06-04 22:13:50 +0300

    fixed some bugs and add button to reset a game

--- src/App.js
+++ src/App.js
@@ -1,63 +1,78 @@
-import { useState, useEffect } from 'react';
-
+import React, { useState, useEffect } from 'react';
 import Cell from "./components/Cell";
 
 const App = () => {
-	const [cells, setCells] = useState(['', '', '', '', '', '', '', '', '']);
-	const [go, setGo] = useState('circle');
-	const [winningMessage, setWinningMessage] = useState(null);
+  const [cells, setCells] = useState(['', '', '', '', '', '', '', '', '']);
+  const [go, setGo] = useState('circle');
+  const [winningMessage, setWinningMessage] = useState(null);
 
-	const message = "It is now " + go + "'s go."
-	console.log(cells);
+  const message = "It is now " + go + "'s go.";
 
-	const checkScore = () => {
-		const winningCombos = [
-			[0, 1, 2], [3, 4, 5], [6, 7, 8],
-			[0, 3, 6], [1, 4, 7], [2, 5, 8],
-			[0, 4, 8], [2, 4, 6]
-		]
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const checkScore = () => {
+    const winningCombos = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6]
+    ];
 
-		winningCombos.forEach(array => {
-			let circleWins = array.every(cell => cells[cell] === 'circle');
+    winningCombos.forEach(array => {
+      let circleWins = array.every(cell => cells[cell] === 'circle');
 
-			if (circleWins) {
-				setWinningMessage('Circle Wins!');
-				return;
-			}
-		});
+      if (circleWins) {
+        setWinningMessage('Circle Wins!');
+        return;
+      }
+    });
 
-		winningCombos.forEach(array => {
-			let crossWins = array.every(cell => cells[cell] === 'cross');
+    winningCombos.forEach(array => {
+      let crossWins = array.every(cell => cells[cell] === 'cross');
 
-			if (crossWins) {
-				setWinningMessage('Cross Wins!');
-				return;
-			}
-		});
-	}
+      if (crossWins) {
+        setWinningMessage('Cross Wins!');
+        return;
+      }
+    });
+  };
 
-	useEffect(() => {
-		checkScore()
-	}, [cells]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const checkDraw = () => {
+    if (cells.every((cell) => cell !== '') && !winningMessage) {
+      setWinningMessage("It's a draw!");
+    }
+  };
 
-	return (
-		<div className="app">
-			<div className='gameboard'>
-				{cells.map((cell, index) => 
-						<Cell 
-							key={index} 
-							id={index} 
-							cell={cell} 
-							setCells={setCells}
-							go={go}
-							setGo={setGo}
-							cells={cells}
-							winningMessage={winningMessage}
-						/>)}
-			</div>
-			<p>{winningMessage || message}</p>
-		</div>
-	);
-}
+  useEffect(() => {
+    checkScore();
+	checkDraw();
+  }, [cells, checkScore, checkDraw]);
+
+  const handleReset = () => {
+    setCells(['', '', '', '', '', '', '', '', '']);
+    setGo('circle');
+    setWinningMessage(null);
+  };
+
+  return (
+    <div className="app">
+      <div className='gameboard'>
+        {cells.map((cell, index) =>
+          <Cell
+            key={index}
+            id={index}
+            cell={cell}
+            setCells={setCells}
+            go={go}
+            setGo={setGo}
+            cells={cells}
+            winningMessage={winningMessage}
+          />
+        )}
+      </div>
+      <p className='message'>{winningMessage || message}</p>
+      <button className='reset' onClick={handleReset}>New Game</button>
+    </div>
+  );
+};
 
 export default App;
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,39 +1,20 @@
-const Cell = ({id, cell, setCells, go, setGo, cells, winningMessage}) => {
-
-    const handleClick = (e) => {
-        const taken = e.target.firstChild.classList.contains('circle') ||
-                      e.target.firstChild.classList.contains('cross')
-
-        if (!taken) {
-            if (go === 'circle') {
-                e.target.firstChild.classList.add('circle');
-                handleCellChange('circle');
-                setGo('cross')
-            }
-            if (go === 'cross') {
-                e.target.firstChild.classList.add('cross');
-                handleCellChange('cross');
-                setGo('circle')
-            }
-        }
-    }
-
-    const handleCellChange = (className) => {
-        const nextCells = cells.map((cell, index) => {
-            if (index === id) {
-                return className;
-            } else {
-                return cell;
-            }
-        })
-        setCells(nextCells);
-    }
-
+const Cell = ({ id, cell, setCells, go, setGo, cells, winningMessage }) => {
+    const handleClick = () => {
+      if (cell === '' && !winningMessage) {
+        const updatedCells = [...cells];
+        updatedCells[id] = go;
+        setCells(updatedCells);
+  
+        setGo(go === 'circle' ? 'cross' : 'circle');
+      }
+    };
+  
     return (
-        <div className='square' id={id} onClick={!winningMessage && handleClick}>
-            <div className={cell}></div>
-        </div>
-    )
-}
-
-export default Cell;
+      <div className="square" id={id} onClick={handleClick}>
+        <div className={`cell ${cell}`}></div>
+      </div>
+    );
+  };
+  
+  export default Cell;
+  
